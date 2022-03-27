15b20d54f7e8729675374364fbe35464610e4a4f
zachkingcade <zachkingcade@gmail.com>
2022-03-27 11:41:43 -0500

    Game finished

--- source/scenes/Dos.js
+++ source/scenes/Dos.js
@@ -25,22 +25,33 @@ class DosScene extends Phaser.Scene {
                 { option: "Assembly", difficulty: 1, cost: 0, costOpportunity: 100 },
                 { option: "C++", difficulty: 3, cost: 0, costOpportunity: 150 }
             ],
-            Advertising: [
+            advertising: [
                 { option: "None", difficulty: 0, cost: 0, costOpportunity: 0 },
                 { option: "Radio", difficulty: 1, cost: 200, costOpportunity: 400 },
                 { option: "Newspaper", difficulty: 1, cost: 100, costOpportunity: 200 },
             ],
         }
-        this.currentGame = {};
+        this.currentGame = { cost: 0 };
         this.gameReady = false;
         this.gameTyping = false;
+        this.gameFin = false;
+        this.gameEndingRan = false;
         this.bytes = 0;
     }
 
     preload() {
+        this.load.image("man", "../../assets/man.png")
+        this.load.audio("key1", "../../assets/key1.mp3")
+        this.load.audio("key2", "../../assets/key2.mp3")
+        this.load.audio("music", "../../assets/music.wav")
+        this.load.audio("fadeIn", "../../assets/fadeIn.wav")
+        this.load.audio("bytes", "../../assets/bytes.wav")
     }
 
     create() {
+        this.sound.play("music",{
+            loop: true
+        })
         //input
         this.up = this.input.keyboard.addKey("UP")
         this.up.on("down", () => {
@@ -69,43 +80,87 @@ class DosScene extends Phaser.Scene {
             color: "lightgreen",
             fontSize: 20,
             lineSpacing: 10
-        }
-        )
+        })
+
+        //money Text
+        this.moneyText = this.add.text(1070, 10, "", {
+            color: "lightgreen",
+            fontSize: 20,
+            lineSpacing: 10
+        })
+        this.moneyText.setOrigin(1, 0)
+
+        this.man = this.add.image(700, 1000, "man")
+            .setOrigin(.5, .5)
+            .setScale(2, 2)
+            .setAlpha(0);
     }
 
     update() {
         this.tick++;
-        if (this.tick % 1 == 0) {
-            this.printChar();
+        if (this.tick % 3 == 0) {
+            this.printChar(this.tick);
+        }
+        if(this.tick % 8 == 0 && this.gameTyping && this.printBuffer[0] != '-'){
+            let soundKey = Math.random() > .5? "key1" : "key2";
+            this.sound.play(soundKey);
         }
         this.gameTyping = this.printBuffer.length > 0;
         //signals the game has finished
         if (this.gameReady & !this.gameTyping) {
-            this.gameReady = false;
-            this.currentGame = {};
+            if (this.bytes > 10000 && !this.gameFin && !this.gameEndingRan) {
+                this.gameWon();
+            } else {
+                this.log("I should make another Program!");
+                this.log("Platform?");
+            }
+                this.sound.play("bytes")
+                this.gameReady = false;
+                this.currentGame = { cost: 0 }
+                this.moneyText.updateText(" ");
+                this.moneyText.setText("Bytes: " + this.bytes.toFixed(2))
+                this.makeChoice("UP");
         }
+        if (!this.gameTyping && this.gameFin && !this.gameEndingRan) {
+            this.gameEndingRan = true;
+            this.sound.play("fadeIn")
+            this.add.tween({
+                targets: this.man,
+                duration: 4000,
+                repeat: 0,
+                alpha: .25,
+                onComplete: () => {
+                    this.log("Well since computer science is the greatest thing in the world\nI might as well continue making projects here in the real world")
+                }
+            })
+        }
+    }
+
+    gameWon() {
+        let sw = "-----------------\n"
+        this.log("\n\nHey" + sw +
+            "I don't know what you did but we have enough bytes to get you out" + sw +
+            "Here we go and....." + sw);
+        this.gameTyping = true;
+        this.gameFin = true;
     }
 
     log(string) {
-        console.log("Logging: ");
         this.printBuffer += '\n' + string;
-        console.log(this.printBuffer);
     }
 
-    printChar() {
-        if (this.printBuffer.length > 0) {
+    printChar(tick) {
+        if (this.printBuffer.length > 0 && this.printBuffer[0] != '-') {
             this.internalLog += this.printBuffer[0];
-            this.printBuffer = this.printBuffer.substr(1);
             this.logText.setText(this.internalLog);
         }
+        this.printBuffer = this.printBuffer.substr(1);
     }
 
     makeChoice(input) {
-        if (!this.gameReady) {
+        if (!this.gameReady && !this.gameTyping) {
             console.log("input = " + input)
             let keys = Object.keys(this.options)
-            console.log(keys);
-            console.log(this.selectedChoice, this.options[keys[this.programmingStage]].length)
             if (input == "UP") {
                 this.selectedChoice++;
                 if (this.selectedChoice > this.options[keys[this.programmingStage]].length - 1) {
@@ -118,31 +173,38 @@ class DosScene extends Phaser.Scene {
                 }
             } else if (input == "ENTER") {
                 console.log("enter hit");
-                this.currentGame[keys[this.programmingStage]] = this.options[keys[this.programmingStage]][this.selectedChoice];
-                this.log(`//Note to self: ${keys[this.programmingStage]} => ${this.options[keys[this.programmingStage]][this.selectedChoice].option}`)
-                this.log(`//Will probably cost me around: $${this.options[keys[this.programmingStage]][this.selectedChoice].cost}`)
-                this.log(`//This choice will probably make this project ${this.options[keys[this.programmingStage]][this.selectedChoice].difficulty}/5 more complex`)
+                console.log(this.options[keys[this.programmingStage]][this.selectedChoice].cost, this.bytes - this.currentGame.cost)
+                if (this.options[keys[this.programmingStage]][this.selectedChoice].cost <= (this.bytes - this.currentGame.cost)) {
+                    this.currentGame[keys[this.programmingStage]] = this.options[keys[this.programmingStage]][this.selectedChoice];
+                    this.log(`//Note to self: ${keys[this.programmingStage]} => ${this.options[keys[this.programmingStage]][this.selectedChoice].option}`)
+                    this.log(`//Will probably cost me around: $${this.options[keys[this.programmingStage]][this.selectedChoice].cost}`)
+                    this.log(`//This choice will probably make this project ${this.options[keys[this.programmingStage]][this.selectedChoice].difficulty}/5 more complex`)
 
-                if (this.programmingStage == 3) {
-                    this.gameReady = true;
-                    this.selectedChoice = 0;
-                    this.programmingStage = 0;
-                    this.cursorText.setText("> ");
-                    let profit = 
-                    this.log("I should be ready to program now!\n\n Let's start!");
-                    this.log(this.create);
-                    //this.log(this.makeChoice);
-                    this.log("Waiting for payment . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .");
-                    this.log("I made $xxxx\n");
+                    if (this.programmingStage == 3) {
+                        this.gameReady = true;
+                        this.selectedChoice = 0;
+                        this.programmingStage = 0;
+                        this.cursorText.setText("> ");
+                        this.currentGame = this.sumGame(this.currentGame);
+                        this.bytes -= Number(this.currentGame.cost)
+                        let profit = (this.currentGame.costOpportunity * this.getRandomNum(.7, .6)).toFixed(2);
+                        this.log("I should be ready to program now!\n\n Let's start!");
+                        this.log(this.sumGame);
+                        this.log("Waiting for payment . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .");
+                        this.log(`I made ${profit}\n`);
+                        this.bytes += Number(profit);
+                    } else {
+                        this.currentGame["cost"] += this.options[keys[this.programmingStage]][this.selectedChoice].cost;
+                        this.selectedChoice = 0;
+                        this.programmingStage++;
+                        this.log("// " + keys[this.programmingStage] + "?")
+                    }
                 } else {
-                    this.selectedChoice = 0;
-                    this.programmingStage++;
-                    this.log("// " + keys[this.programmingStage] + "?")
+                    this.log("I don't have enough bytes for that!");
                 }
-                console.log(this.currentGame);
             }
             if (this.gameReady == false) {
-                this.cursorText.setText("> " + this.options[keys[this.programmingStage]][this.selectedChoice].option)
+                this.cursorText.setText("> " + this.options[keys[this.programmingStage]][this.selectedChoice].option + "\t" + "Cost: " + this.options[keys[this.programmingStage]][this.selectedChoice].cost)
             }
         }
     }
@@ -151,7 +213,45 @@ class DosScene extends Phaser.Scene {
         return base + (Math.random() * deviation);
     }
 
+    sumGame(game) {
+        let cost = 0;
+        let difficulty = 0;
+        let possibleProfit = 0;
+        cost += game["platform"].cost;
+        cost += game["topic"].cost;
+        cost += game["language"].cost;
+        cost += game["advertising"].cost;
+        difficulty += game["platform"].difficulty;
+        difficulty += game["topic"].difficulty;
+        difficulty += game["language"].difficulty;
+        difficulty += game["advertising"].difficulty;
+        possibleProfit += game["platform"].costOpportunity;
+        possibleProfit += game["topic"].costOpportunity;
+        possibleProfit += game["language"].costOpportunity;
+        possibleProfit += game["advertising"].costOpportunity;
+        game["cost"] = cost;
+        game["difficulty"] = difficulty;
+        game["costOpportunity"] = possibleProfit;
+        console.log()
+        return game;
+    }
+
     startup() {
+        let sw = "-----------------\n"
+        this.log("Hello\n" + sw + sw +
+            "Can you hear me?\n" + sw +
+            "You've been trapped in this computer by the evil Rick Astley\n" + sw +
+            "We'll try to get you out but we're low on funds to research the issue" + sw +
+            "Maybe you can help from your end, we're only 10,000 bytes short (local currency)" + sw +
+            "I found a file that might help you\n\n" + sw +
+            "cd ./ACM_Game_Jam_Spring_2022/secrets/cia/super_Don't_touch" + sw +
+            "read help.txt" + sw +
+            "To design a sortware tool use the arrow keys to select key componets when prompted\n" +
+            "after you have made the software tools the software will automatically go to market\nand be sold" +
+            " make enough money and maybe I will let you out, -Astley\n" + sw +
+            "I don't know what that means but we need to go get started on research\n" + sw +
+            "Good Luck!-----------------------\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+        //Dos Startup
         this.log("Welcome to GJ-Dos\n" +
             "V1.9.1\n" +
             "Detected Devices:\n" +
